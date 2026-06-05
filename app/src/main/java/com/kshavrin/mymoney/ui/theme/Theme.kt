7b0c33d99786690282c1743a2d8b41300cda8430
Kirill Shavrin <desvingns@gmail.com>
2026-06-05 18:24:42 +0200

    feat: add M3 design tokens - app theme bootstrap

---
+++ app/src/main/java/com/kshavrin/mymoney/ui/theme/Theme.kt
@@ -0,0 +1,43 @@
+package com.kshavrin.mymoney.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun MyMoneyTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    val context = LocalContext.current
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            if (darkTheme) {
+                dynamicDarkColorScheme(context)
+            } else {
+                dynamicLightColorScheme(context)
+            }
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    CompositionLocalProvider(
+        LocalSpacing provides Spacing(),
+        LocalMotion provides Motion(),
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = AppTypography,
+            shapes = AppShapes,
+            content = content,
+        )
+    }
+}

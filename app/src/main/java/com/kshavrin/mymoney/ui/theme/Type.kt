1be50ee48cdf145be353a677cf64be9d9ffa9c8e
Kirill Shavrin <desvingns@gmail.com>
2026-06-05 11:54:33 +0200

    Revert "feat: bootstrap app ui theme tokens"
    This reverts commit b6e0ae248aa2e5ed5842f9d055a1ba11eef6a1ec.

--- app/src/main/java/com/kshavrin/mymoney/ui/theme/Type.kt
+++
@@ -1,6 +0,0 @@
-package com.kshavrin.mymoney.ui.theme
-
-import androidx.compose.material3.Typography
-import com.kshavrin.mymoney.core.ui.theme.MoneyTypography
-
-val AppTypography: Typography = MoneyTypography

500472e3ce2b85e78e2957d21637909ce7417881
Kirill Shavrin <desvingns@gmail.com>
2026-06-06 21:56:24 +0200

    test: fix transaction form ui test imports

--- core/designsystem/src/androidTest/java/com/kshavrin/mymoney/core/designsystem/form/TransactionFormContentUiTest.kt
+++ core/designsystem/src/androidTest/java/com/kshavrin/mymoney/core/designsystem/form/TransactionFormContentUiTest.kt
@@ -1,15 +1,12 @@
 package com.kshavrin.mymoney.core.designsystem.form
 
 import androidx.compose.ui.test.assertCountEquals
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodes
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
